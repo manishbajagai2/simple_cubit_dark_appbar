3114a80f242d2c9afb3dce10481ea3359f978633
manishbajagai2 <manishbajagai2@gmail.com>
2024-12-21 03:37:20 +0530

    Adding initial screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: Text(
+          'Test',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
